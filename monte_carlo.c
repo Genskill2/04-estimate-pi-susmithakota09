5a7c788313c15e51df3134bdc8e03bc8cc28e9fc
susmithakota09 <85020518+susmithakota09@users.noreply.github.com>
2021-06-04 11:32:59 +0530

    monte_carle.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -38,6 +38,38 @@ int main(void) {
     }
   }
 }
-
-
-
+float mc_pi(int n){
+  float x,y;
+  int i=0,circle=0;
+  float z;
+  int square=0;
+  
+  float pi;
+  
+  while (i<n-1){
+   
+   x=frandom();
+   y=frandom();
+   z=x*x+y*y;
+  
+   
+   if(z<=1){
+     circle+=1;
+     square+=1;
+     }
+   else{
+     square+=1;
+   
+   i++;
+     
+   }
+   
+   }
+ 
+ pi=(float)4*circle/square;
+ return pi;
+   
+    
+  
+  
+}
